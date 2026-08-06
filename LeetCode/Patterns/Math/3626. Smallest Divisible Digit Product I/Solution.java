827e367e81db488367bba5d543a59914c220aba9
Azeem M S <azeemazeem777777777@gmail.com>
2026-08-06 22:16:04 +0530

    Add Java Solution to Patterns/Math

---
+++ LeetCode/Patterns/Math/3626. Smallest Divisible Digit Product I/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while(num(n)%t!=0){
+            n++;
+        }
+        return n;
+    }
+    private int num(int a){
+            int val=1,r;
+            while(a>0)
+            {
+                r=a%10;
+                val=val*r;
+                a=a/10;
+            }
+            return val;
+    }
+}
