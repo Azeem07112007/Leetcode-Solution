86dabae8dbf2bdd03d2f52a859cd9cac14aabd9b
Azeem M S <azeemazeem777777777@gmail.com>
2026-08-07 22:15:30 +0530

    Add Java Solution to Patterns/Two Pointers

---
+++ LeetCode/Patterns/Two Pointers/2000. Reverse Prefix of Word/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+        if (index == -1) {
+            return word;
+        }
+        StringBuilder prefix = new StringBuilder(word.substring(0, index + 1));
+        return prefix.reverse().toString() + word.substring(index + 1);
+    }
+}
